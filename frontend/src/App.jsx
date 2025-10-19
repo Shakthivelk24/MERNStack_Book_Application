8421cc0353e03892c242898fd4c54875970af26f
Shakthivelk24 <shakthivelk1124@gmail.com>
2025-10-19 17:55:31 +0530

    added all components to App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,15 @@
+import Banner from "./components/Banner"
+import Footer from "./components/Footer"
 import Navbar from "./components/Navbar"
-
+import FreeBook from "./components/FreeBook"
 export default function App() {
   return (
+    <>
    <Navbar/> 
+   <Banner/>
+   <FreeBook/>
+   <Footer/>
+    
+   </>
   )
 }
