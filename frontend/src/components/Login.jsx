c964c1dd9d6ff475e450d3015a2b919adfb70a26
Shakthivelk24 <shakthivelk1124@gmail.com>
2025-10-20 15:32:22 +0530

    Designed the Login parts

---
+++ frontend/src/components/Login.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+
+export default function Login() {
+  return (
+    <div>
+      {/* You can open the modal using document.getElementById('ID').showModal() method */}
+
+      <dialog id="my_modal_3" className="modal">
+        <div className="modal-box">
+          <form method="dialog">
+            {/* if there is a button in form, it will close the modal */}
+            <button className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">
+              ✕
+            </button>
+          </form>
+          <h3 className="font-bold text-lg">Login</h3>
+          <div className="mt-4 space-y-2">
+            <span>Email</span>
+            <br />
+            <input type="email"
+            placeholder="Enter your email"
+            className="w-80 px-3 border rounded-md outline-none" />
+          </div>
+        </div>
+      </dialog>
+    </div>
+  );
+}
