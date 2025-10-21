9288d65dce45754673a725ed2b5eccadda225a3d
Shakthivelk24 <shakthivelk1124@gmail.com>
2025-10-21 14:47:00 +0530

    created router for SignUp part

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,6 @@
 import Home from "./Home/Home";
 import Courses from "./courses/Courses";
+import SignUp from "./components/SignUp";
 import {BrowserRouter as Router,Routes,Route} from "react-router-dom";
 export default function App() {
   return (
@@ -8,6 +9,7 @@ export default function App() {
       <Routes>
         <Route path="/" element={<Home/>}/>
         <Route path="/course" element={<Courses/>}/>
+        <Route path="/signup" element={<SignUp/>}/>
       </Routes>
      </Router>
    </>
